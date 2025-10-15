b8f862bb6a28217e6dfae70153192497f56571b2
Gabriel4357 <gabrielseethor.jh@gmail.com>
2025-10-15 22:02:47 +0800

    Initialise supabase instances

--- backend/services/user-service/src/supabase/supabase.module.ts
+++ backend/services/user-service/src/supabase/supabase.module.ts
@@ -1,9 +1,36 @@
-// src/supabase/supabase.module.ts
-import { Module } from "@nestjs/common";
-import { SupabaseService } from "./supabase.service";
+// supabase.module.ts
+import { Global, Module } from "@nestjs/common";
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
+export const SUPABASE = Symbol("SUPABASE");
+export const SUPABASE_ADMIN = Symbol("SUPABASE_ADMIN");
+
+@Global()
 @Module({
-  providers: [SupabaseService],
-  exports: [SupabaseService],
+  providers: [
+    {
+      provide: SUPABASE,
+      useFactory: (): SupabaseClient => {
+        const SUPABASE_URL = process.env.SUPABASE_URL;
+        const SUPABASE_KEY = process.env.SUPABASE_KEY;
+        if (!SUPABASE_URL || !SUPABASE_KEY)
+          throw new Error("Missing SUPABASE_URL or SUPABASE_KEY in env file");
+        return createClient(SUPABASE_URL, SUPABASE_KEY, {});
+      },
+    },
+    {
+      provide: SUPABASE_ADMIN,
+      useFactory: (): SupabaseClient => {
+        const SUPABASE_URL = process.env.SUPABASE_URL;
+        const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
+        if (!SUPABASE_URL || !SUPABASE_SERVICE_KEY)
+          throw new Error(
+            "Missing SUPABASE_URL or SUPABASE_SERVICE_KEY in env file"
+          );
+        return createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY, {});
+      },
+    },
+  ],
+  exports: [SUPABASE, SUPABASE_ADMIN],
 })
 export class SupabaseModule {}
--- backend/services/user-service/src/users/users.service.ts
+++ backend/services/user-service/src/users/users.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import { User } from "./interface/user.interface";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { SUPABASE, SUPABASE_ADMIN } from "../supabase/supabase.module";
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [];
+  constructor(
+    @Inject(SUPABASE) private readonly supabase: SupabaseClient,
+    @Inject(SUPABASE_ADMIN) private readonly supabaseAdmin: SupabaseClient
+  ) {}
 
-  register() {}
+  async register() {
+    const { data, error } = await this.supabase.auth.signUp({
+      email: "example@email.com",
+      password: "example-password",
+    });
+  }
 
   findAll(): User[] {
-    return this.users;
+    return [];
   }
 }
