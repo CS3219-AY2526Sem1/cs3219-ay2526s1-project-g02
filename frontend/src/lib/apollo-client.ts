7f0e3c354df7b567b85924c983b06ab4e34bac38
TinyGambe <developer.juliusbryangambe@gmail.com>
2025-11-04 20:27:08 +0800

    add clients and query

--- frontend/src/lib/apollo-client.ts
+++ frontend/src/lib/apollo-client.ts
@@ -13,3 +13,48 @@ export const client = new ApolloClient({
     },
   },
 });
+
+// Collaboration service client
+const collaborationHttpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_COLLABORATION_GRAPHQL_URL || 'http://localhost:4004/graphql',
+});
+
+export const collaborationClient = new ApolloClient({
+  link: collaborationHttpLink,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
+
+// Question service client
+const questionHttpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_QUESTION_GRAPHQL_URL || 'http://localhost:4002/graphql',
+});
+
+export const questionClient = new ApolloClient({
+  link: questionHttpLink,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
+
+// User service client
+const userHttpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_USER_GRAPHQL_URL || 'http://localhost:4001/graphql',
+});
+
+export const userClient = new ApolloClient({
+  link: userHttpLink,
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
